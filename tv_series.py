187bd7794b7c00e96101a0ce6b555024f0d81eba
CristaudoGiuseppe <gcristaudo98@gmail.com>
2022-12-05 16:54:42 +0100

    Cleaned Code

--- tv_series.py
+++ tv_series.py
@@ -5,65 +5,85 @@ from sklearn.ensemble import GradientBoostingRegressor
 import matplotlib.pyplot as plt
 
 
-API_KEY = 'a7e6c8ae91907ba3f0a6c146319034ae'
-
+with open('settings.json') as settings:
+  settings_json = json.load(settings)
+  API_KEY = settings_json['API_KEY']
+  
 s = requests.session()
 
-def test_api_key(api_key):
-    URL = f'https://api.themoviedb.org/3/movie/76341?api_key={api_key}'
-    r = s.get(URL)
-    print(r.text)
+def test_api_key(api_key = API_KEY):
+    r = (s.get(f'https://api.themoviedb.org/3/movie/76341?api_key={api_key}').text)
+    #print(r_json)
+    try:
+        # wrong key error: {"status_code":7,"status_message":"Invalid API key: You must be granted a valid key.","success":false}
+        if r['success'] == False:
+            print(r['status_message'])
+    except: # Everything Fine
+        print('The API KEY is valid!')
     
-def get_session(api_key):
-    r = s.get(f'https://api.themoviedb.org/3/authentication/token/new?api_key={api_key}')
-    # print(r.text)
-    # {"success":true,"expires_at":"2022-12-02 11:21:15 UTC","request_token":"f2447b37e645a48d0ea703639e3119ab0fadfa9d"}
-    request_token = json.loads(r.text)['request_token']
-    print(request_token)
-    r = s.post(f'https://api.themoviedb.org/3/authentication/session/new?api_key={api_key}', data = {"request_token": request_token})
-    print(r.text)
-    
-def search_keywords(api_key = API_KEY, keyword = ""):
-    r = s.get(f'https://api.themoviedb.org/3/search/tv?query={keyword}&api_key={api_key}&page=1&include_adult=true')
-    r_json = json.loads(r.text)
-    #print(r.text)
+# Search a TV Series by keywords and return the most popular result.
+def search_keywords(keyword, api_key = API_KEY):
+    r = json.loads(s.get(f'https://api.themoviedb.org/3/search/tv?query={keyword}&api_key={api_key}&page=1&include_adult=true').text)
     df = pd.DataFrame(columns=['ID', 'Name', 'Overview', 'Poster', 'Popularity'])
-    for result in r_json['results']:
-        #print(result)
+    
+    # For each result in the response, populate the dataframe.
+    for result in r['results']:
         df.loc[len(df.index)] = [result['id'], result['name'], result['overview'], f"https://image.tmdb.org/t/p/w440_and_h660_face/{result['poster_path']}", result['popularity']]
+    
+    # Drop NaN value, sort by descending popularity and reset DataFrame index
     df = df.dropna()
     df = df.sort_values(by=['Popularity'], ascending=False)
     df = df.reset_index()
-    df = df.head(9)
-    #print(df)
-    return df
+    
+    return df[0]
 
+# Get the informaion on how long episodes are long given a TV Series ID
 def get_episode_runtime(tv_series_id, api_key = API_KEY):
-    r = s.get(f'https://api.themoviedb.org/3/tv/{tv_series_id}?api_key={api_key}')
-    r_json = json.loads(r.text)
-    episode_rt = str(r_json['episode_run_time']).replace('[','').replace(']','')
+    r = json.loads(s.get(f'https://api.themoviedb.org/3/tv/{tv_series_id}?api_key={api_key}').text)
+    
+    # The information about episode runtime is encoded as [22] or [50, 60] 
+    episode_rt = str(r['episode_run_time']).replace('[','').replace(']','') # [22] -> 22
     if ',' in episode_rt:
-        episode_rt = episode_rt.split(',')[0]
+        episode_rt = episode_rt.split(',')[0] # 50, 60 -> 50
+        
     return int(episode_rt)
+
+# Return a Python Dictionary containing minor information about a TV Series
+def get_tv_series_general_information(r, tv_series_id):
+    result = {
+        'vote_avg'              : r['vote_average'],
+        'vote_count'            : r['vote_count'],
+        'episode_rt'            : get_episode_runtime(tv_series_id),
+        'in_production'         : r['in_production'],
+        'number_of_episodes'    : r['number_of_episodes'],
+        'number_of_seasons'     : r['number_of_seasons']
+    }
     
+    return result
+
+# Get more information about a TV Series given a tv_series_id
+def get_tv_series_overview(tv_series_id, api_key = API_KEY):
+    r = json.loads(s.get(f'https://api.themoviedb.org/3/tv/{tv_series_id}?api_key={api_key}').text)
+    minor_information_dict = get_tv_series_general_information(r, tv_series_id)
     
-def get_tv_series_overview(id, api_key = API_KEY):
-    r = s.get(f'https://api.themoviedb.org/3/tv/{id}?api_key={api_key}')
-    r_json = json.loads(r.text)
-    vote_avg = r_json['vote_average']
-    vote_count = r_json['vote_count']
-    episode_rt = get_episode_runtime(id)
-    in_production = r_json['in_production']
-    number_of_episodes = r_json['number_of_episodes']
-    number_of_seasons = r_json['number_of_seasons']
     df = pd.DataFrame(columns=['ID', 'Name', 'Air Date', 'Season Number', 'Poster', 'Episode Count'])
-    for season in r_json['seasons']:
+    
+    for season in r['seasons']:
         df.loc[len(df.index)] = [season['id'], season['name'], season['air_date'], season['season_number'], f"https://image.tmdb.org/t/p/w440_and_h660_face/{season['poster_path']}" , season['episode_count']]
     
-    return vote_avg, vote_count, episode_rt, in_production, number_of_episodes, number_of_seasons, df
+    return minor_information_dict, df
 
-# get watch providers
-# https://api.themoviedb.org/3/tv/456/watch/providers?api_key=a7e6c8ae91907ba3f0a6c146319034ae
+# Get watch providers information about a TV Series given a tv_series_id
+def get_tv_series_watch_providers(tv_series_id, api_key = API_KEY):
+    r = json.loads(s.get(f'https://api.themoviedb.org/3/tv/{tv_series_id}/watch/providers?api_key={api_key}').text)
+    
+    df = pd.DataFrame(columns=['Country', 'Provider Name', 'Provider Logo'])
+    
+    for res in r['results']:
+        # {'link': 'https://www.themoviedb.org/tv/456-the-simpsons/watch?locale=ZA', 'flatrate': [{'logo_path': '/7rwgEs15tFwyR9NPQ5vpzxTj19Q.jpg', 'provider_id': 337, 'provider_name': 'Disney Plus', 'display_priority': 28}]}
+        df.loc[len(df.index)] = [res, r['results'][res]['flatrate'][0]['provider_name'], f"https://image.tmdb.org/t/p/w440_and_h660_face/{r['results'][res]['flatrate'][0]['logo_path']}"]
+    
+    return df
 
 def get_heatmap_avg_vote(seasons):
     df = pd.DataFrame()
@@ -188,6 +208,7 @@ def get_season_overview(tv_show_id, season_number, api_key = API_KEY):
 #search_keywords(API_KEY, 'sex education')
 # get_tv_series_overview(API_KEY, 456)
 # get_season_overview(API_KEY, 456, 2)
-get_series_data(81356)
-# wrong key error: {"status_code":7,"status_message":"Invalid API key: You must be granted a valid key.","success":false}
+#get_series_data(81356)
+#test_api_key()
+get_tv_series_watch_providers(456)
     
